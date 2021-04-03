fe49f9e2fb59e5d37f2e2a3a61bb659f6c157e41
Jack Boettcher <jcboettcher@college.harvard.edu>
2021-04-02 19:36:30 -0500

    test

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to Harvard Puzzle Hunt</h1>
+      <h1>Welcome to Harvard Puzzle Hunt twice!</h1>
     </div>
   )
 }
